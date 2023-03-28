bab5b8743fa479b3723a830bcff5a0ef3fd3161f
Pheeraphat <dpheeraphat2547@gmail.com>
2023-03-28 21:42:10 +0700

    add interest

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Frontend Developer" , "Software Engineer"];
+  const toRotate = ["Frontend Developer" , "Software Engineer" , "Product Management"];
   const period = 300;
 
   useEffect(() => {
